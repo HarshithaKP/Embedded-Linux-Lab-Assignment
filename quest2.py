4d1bf65ec013a215efd0760c75d0ae0a01bbfbe4
HarshithaKP <42645847+HarshithaKP@users.noreply.github.com>
2018-10-16 15:49:22 +0530

    Update quest2.py

--- quest2.py
+++ quest2.py
@@ -10,8 +10,12 @@ def connection():
 	else:
 		print("Not connected")
 
-def Insertion(ID,Fname,Lname,DOB):
-	query="insert into users181041008('ID','Fname','Lname','DOB') values('%s','%s','%s','%s')"
+def Insertion():
+	ID=input("Enter ID : ")
+	Fname=input("Enter First name : ")
+	Lname=input("Enter Last name : ")
+	DOB=input("Enter DOB : ")
+	query="INSERT INTO users181041008(ID,Fname,Lname,DOB) VALUES(%s,%s,%s,%s)"
 	args=(ID,Fname,Lname,DOB);
 	mycursor = conn.cursor();	
 	mycursor.execute(query,args);
@@ -21,48 +25,47 @@ def Insertion(ID,Fname,Lname,DOB):
 
 
 def Retrive_all():
-	query="Select *from users181041008"
+	query="SELECT *from users181041008"
+	mycursor = conn.cursor();
+	mycursor.execute(query);
+	rows = mycursor.fetchall();
+	print("No of Record: ", mycursor.rowcount)
+	#print(rows)
+	for row in rows:
+		print(row);
+
+
+
+
+def Alter_table(attribute):
+	query="ALTER TABLE users181041008 ADD %s varchar(50)"%(attribute)
+	#args=(attribute);
 	mycursor=conn.cursor();
 	mycursor.execute(query);
-	rows=cursor.fetchall();
-
-	for row in rows:
-		print(row)
-
-
-
-
-def Alter_table():
-	query="Alter users181041008 add constraint %s"
-	args=(attribute);
-	mycursor=conn.cursor();
-	mycursor.execute(query,args);
+	print("Successfully altered ")
 	conn.commit();
 
 
 
-
-
-
 def main():
-	connection()
-	print("Choose an operation to perform ")
-	print("1:Insertion \n")
-	print("2:Retrieve all \n")
-	print("3:Alter Table \n")
-	print("q:Quit \n")
-	option=input("Select any : ")
 	while(True):
+		connection()
+		print("Choose an operation to perform ")
+		print("1:Insertion \n")
+		print("2:Retrieve all \n")
+		print("3:Alter Table \n")
+		print("q:Quit \n")
+		option=input("Select any : ")
+	
 		if(option=='1'):
-			ID=input("Enter ID : ")
-			Fname=input("Enter First name : ")
-			Lname=input("Enter Last name : ")
-			DOB=input("Enter DOB : ")
-			Insertion(ID,Fname,Lname,DOB)
+			Insertion();
 		elif(option=='2'):
 			Retrive_all()
+			
 		elif(option=='3'):
-			Alter_table()
+			attribute=input("Mention the attribute to add in table :")
+			Alter_table(attribute)
+			
 		elif(option=='q'):
 			sys.exit()
 		else:
